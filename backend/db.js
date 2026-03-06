f23c56fd684ecef7bfba9fc32a2b566757bdd5c4
Manohar1316 <mudragadamanohar1316@gmail.com>
2026-03-06 10:17:33 +0530

    update db config for deployment

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,16 @@
-const mysql=require("mysql");
+const mysql = require("mysql");
 
-const db=mysql.createConnection({
-host:"localhost",
-user:"root",
-password:"2004",
-database:"student_portal"
+const db = mysql.createConnection({
+  host: process.env.MYSQLHOST || "localhost",
+  user: process.env.MYSQLUSER || "root",
+  password: process.env.MYSQLPASSWORD || "2004",
+  database: process.env.MYSQLDATABASE || "student_portal",
+  port: process.env.MYSQLPORT || 3306
 });
 
-db.connect(err=>{
-if(err) console.log(err);
-else console.log("MySQL Connected");
+db.connect(err => {
+  if (err) console.log(err);
+  else console.log("MySQL Connected");
 });
 
-module.exports=db;
+module.exports = db;
